a3b0ad4c0bcb690f06824d3b17f921ce6712c76b
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-12-10 10:52:27 +0000

    passed the minimum thermostat temperature and also returned a string telling you the temperature

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,19 @@
+"use strict";
+
 class Thermostat{
   constructor(){
     this.temperature = 20;
+    this.minimumTemperature = 10;
   }
     up(temperature){
       this.temperature += temperature;
     }
     down(temperature){
       this.temperature -= temperature;
+      if(this.temperature < this.minimumTemperature){
+        this.temperature = this.minimumTemperature
+        return "The temperature cannot go lower than 10 degrees"
+      }
+      return `The temperature is ${this.temperature} degrees`
     }
 }
