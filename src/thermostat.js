da7a373527fa5da26f0c159ffa86b27d0dae0472
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-12-10 11:45:15 +0000

    power saving mode is off test

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,9 +5,19 @@ class Thermostat{
     this.temperature = 20;
     this.minimumTemperature = 10;
     this.power_saving_mode = true;
+    this.maximumTemperature = 25;
   }
     up(temperature){
       this.temperature += temperature;
+      if(this.power_saving_mode = true && this.temperature > this.maximumTemperature) {
+           this.temperature = this.maximumTemperature;
+           return "Power saving mode is on. Now moving to the maximum of 25 degrees."
+       }
+       else if(this.power_saving_mode = false && this.temperature > this.maximumTemperature) {
+           this.temperature = this.maximumTemperature;
+           return "Power saving mode is off. Now moving to the maximum of 32 degrees."
+       }
+       return `The temperature is now ${this.temperature} degrees.`;
     }
     down(temperature){
       this.temperature -= temperature;
